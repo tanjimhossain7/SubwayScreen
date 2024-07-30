0ef3bcd477af1f7f9be1bbc3cebb5ab6e87f6c62
MD Tanjim Hossain <mdtanjim.hossain@ucalgary.ca>
2024-07-30 03:07:17 -0600

    Added WeatherFetcher class and MainApp entry point

--- src/ca/ucalgary/edu/ensf380/MainApp.java
+++ src/ca/ucalgary/edu/ensf380/MainApp.java
@@ -1,5 +1,15 @@
 package ca.ucalgary.edu.ensf380;
 
-public class MainApp {
+import java.io.IOException;
 
+public class MainApp {
+    public static void main(String[] args) {
+        try {
+            String weatherData = WeatherFetcher.fetchWeather("Calgary");
+            String weatherInfo = WeatherFetcher.parseWeatherData(weatherData);
+            System.out.println(weatherInfo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
