8ab28c4e028c4813ce591cc11e3c23e23cae3fb7
MD Tanjim Hossain <mdtanjim.hossain@ucalgary.ca>
2024-08-02 23:23:58 -0600

    Updates

--- src/ca/ucalgary/edu/ensf380/MainApp.java
+++ src/ca/ucalgary/edu/ensf380/MainApp.java
@@ -4,10 +4,22 @@ import java.io.IOException;
 
 public class MainApp {
     public static void main(String[] args) {
+        // Fetch and display weather data
         try {
             String weatherData = WeatherFetcher.fetchWeather("Calgary");
             String weatherInfo = WeatherFetcher.parseWeatherData(weatherData);
+            System.out.println("Weather Information:");
             System.out.println(weatherInfo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Fetch and display news data
+        try {
+            String newsData = NewsFetcher.fetchNews();
+            String newsInfo = NewsFetcher.parseNewsData(newsData);
+            System.out.println("\nNews Information:");
+            System.out.println(newsInfo);
         } catch (IOException e) {
             e.printStackTrace();
         }
